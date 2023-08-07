3558705d892def9c0f688de6d0be473eefbfa13a
Naglaa Saeed <naglaasaeedalgzar@gmail.com>
2023-08-07 15:34:36 +0300

    add VCM

---
+++ lib/View/OnboardingScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding_Screen extends StatefulWidget {
+  const Onboarding_Screen({super.key});
+
+  @override
+  State<Onboarding_Screen> createState() => _Onboarding_ScreenState();
+}
+
+class _Onboarding_ScreenState extends State<Onboarding_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
