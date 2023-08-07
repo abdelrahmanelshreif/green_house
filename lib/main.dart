3827a170757d9579268797f9d3f3e278aa83ea02
Abdelrahman Elshreif <link93955@gmail.com>
2023-08-08 02:48:23 +0300

    UI SCREEN OF PRODUCT DETAILS "STATIC NO ACTIONS"

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:green_house/View/OnboardingScreen.dart';
+
+// import 'package:green_house/View/OnboardingScreen.dart';
+import 'package:green_house/View/planet_details_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: PlanetDetailsScreen();,
+//     );
+//   }
+// }
 
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home: Onboarding_Screen(),
-    );
-        }
+        debugShowCheckedModeBanner: false,
+        // home: HomeScreen(),
+        home: PlanetDetailsScreen());
+  }
 }
