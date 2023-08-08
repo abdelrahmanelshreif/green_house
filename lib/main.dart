69c1701deec0328d6a0b5770ced51aa35ae7c3ae
Naglaa Saeed <naglaasaeedalgzar@gmail.com>
2023-08-08 03:08:56 +0300

    onboardingScreen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: Onboarding_Screen(),
+      home: OnboardingScreen(num: 1),
     );
         }
 }
